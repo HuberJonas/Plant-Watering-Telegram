5e6fa5fa31650c07f968d0ea0f5970f4c887f229
Jonas Huber <huber_jonas@icloud.com>
2022-05-29 10:37:06 +0200

    bugfix

--- src/eeprom_handler.h
+++ src/eeprom_handler.h
@@ -3,56 +3,36 @@
 
 /**
  * EEPROM structure
- * 0    moistureThreshold (first part   value: 1)
- * 1    moistureThreshold (second part  value: 255)
- * 2    volume (first part      value: 1)
- * 3    volume (second part     value: 255)
- * 4    amount registered users
+ * 0-1  readingAir (value 1, 255)
+ * 2-3  readingWater (value 1, 255)
+ * 4    threshold
+ * 5-6  volume (value 1, value 255)
+ * 7    amount registered users
  * 
  * 10-24    ChatID User1
  * ...
  */
 
-int readMoistureThreshold() {
-    int first = EEPROM.read(0);
-    int second = EEPROM.read(1);
-    int threshold = first + second*256;
-    return threshold;
+
+/**
+ * 
+ * EEPROM interface functions
+ *
+ */
+int readNumberTwoParts(int index) {
+    int first = EEPROM.read(index);
+    int second = EEPROM.read(index+1);
+    return first + second*256;
 }
 
-void writeMoistureThreshold(int threshold) {
-    int first = threshold%256;
-    int second = int((threshold-first)/256);
-    EEPROM.write(0, first);
-    EEPROM.write(1, second);
+void writeNumberTwoParts(int index, int value) {
+    int first = value%256;
+    int second = int((value-first)/256);
+    EEPROM.write(index, first);
+    EEPROM.write(index+1, second);
     EEPROM.commit();
 }
 
-
-int readVolume() {
-    int first = EEPROM.read(2);
-    int second = EEPROM.read(3);
-    int volume = first + second*256;
-    return volume;
-}
-
-void writeVolume(int volume) {
-    int first = volume%256;
-    int second = (volume-first)/256;
-    EEPROM.write(2, first);
-    EEPROM.write(3, second);
-    EEPROM.commit();
-}
-
-
-int readAmountRegisteredUsers() {
-    return EEPROM.read(4);
-}
-
-void writeAmountRegisteredUsers(int amount) {
-    EEPROM.write(4, amount);
-    EEPROM.commit();
-}
 
 
 String readString(int address) {
@@ -76,18 +56,76 @@ void writeString(int address, String content) {
 }
 
 
+
+void resetEEPROM() {
+    for(unsigned int i=0; i<EEPROM.length(); i++) {
+        EEPROM.write(i, 0);
+    }
+    EEPROM.commit();
+}
+
+
+/**
+ *
+ * Read/Write functions
+ * 
+ */
+int readReadingAir() {
+    return readNumberTwoParts(0);
+}
+
+void writeReadingAir(int value) {
+    writeNumberTwoParts(0, value);
+}
+
+
+
+int readReadingWater() {
+    return readNumberTwoParts(2);
+}
+
+void writeReadingWater(int value) {
+    writeNumberTwoParts(2, value);
+}
+
+
+
+int readMoistureThreshold() {
+    return EEPROM.read(4);
+}
+
+void writeMoistureThreshold(int value) {
+    EEPROM.write(4, value);
+    EEPROM.commit();
+}
+
+
+
+int readVolume() {
+    return readNumberTwoParts(5);
+}
+
+void writeVolume(int value) {
+    writeNumberTwoParts(5, value);
+}
+
+
+
+int readAmountRegisteredUsers() {
+    return EEPROM.read(4);
+}
+
+void writeAmountRegisteredUsers(int amount) {
+    EEPROM.write(4, amount);
+    EEPROM.commit();
+}
+
+
+
 String readChatID(int index) {
     return readString(index*15+10);
 }
 
 void writeChatID(int index, String chatID) {
     writeString(index*15+10, chatID);
-}
-
-
-void resetEEPROM() {
-    for(int i=0; i<EEPROM.length(); i++) {
-        EEPROM.write(i, 0);
-    }
-    EEPROM.commit();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,10 @@
 void checkMoisture();
 void waterPlant();
 int readMoisture(int amountReadings);
+int readSensorRaw(int amountReadings);
 void updateBot();
 void handleMessage(String chatID, String text, String name);
-void notifyUsers();
+void notifyUsers(String message);
 boolean verifyUser(String chatID);
 void addUser(String chatID);
 void deleteUser(String chatID);
@@ -19,11 +20,16 @@ void deleteUser(String chatID);
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 WiFiClientSecure clientSecure;
 UniversalTelegramBot bot(BOT_TOKEN, clientSecure);
-boolean usersHaveBeenNotified = false;
+boolean usersHaveBeenNotifiedTank = false;
+boolean usersHaveBeenNotifiedTankEmpty = false;
+boolean usersHaveBeenNotifiedCalibration = false;
 unsigned long lastMillisUpdateBot;
 unsigned long lastMillisCheckMoisture;
+unsigned long lastMillisWatering;
 
 int threshold = 0;
+int readingAir = 0;
+int readingWater = 0;
 int tankFill = 0;
 int amountRegisteredUsers = 0;
 String users[AMOUNT_USERS];
@@ -49,20 +55,24 @@ void setup() {
     delay(500);
   }
   Serial.println(" Success");
+  delay(100);
 
   // Pump and EEPROM
   pinMode(PUMP_PIN, OUTPUT);
-  digitalWrite(PUMP_PIN, HIGH);
+  digitalWrite(PUMP_PIN, LOW);
   EEPROM.begin(AMOUNT_USERS*15+10);
 
   threshold = readMoistureThreshold();
+  readingAir = readReadingAir();
+  readingWater = readReadingWater();
   tankFill = readVolume();
-  tankFill = 100;
+  Serial.println("DEBUG: Started EEPROM");
 
   amountRegisteredUsers = readAmountRegisteredUsers();
   for(int i=0; i<amountRegisteredUsers; i++) {
     users[i] = readChatID(i);
   }
+  Serial.println("Ready to be used!");
 }
 
 
@@ -72,7 +82,7 @@ void loop() {
     lastMillisUpdateBot = millis();
   }
 
-  if(millis() - lastMillisCheckMoisture >= UPDATE_INTERVAL) {
+  if(millis() - lastMillisCheckMoisture >= 5000) {
     checkMoisture();
     lastMillisCheckMoisture = millis();
   }
@@ -90,29 +100,39 @@ void checkMoisture() {
   int reading = readMoisture(3);
   
   // water if necessary
-  if(reading < threshold) {
+  if(reading < threshold && millis() - lastMillisWatering >= PUMP_WAIT) {
     waterPlant();
   }
 
   // check if water level is critical
-  if(tankFill <= PUMP_VOLUME*2 && usersHaveBeenNotified == false) {
+  if(tankFill < TANK_VOLUME*0.1 && usersHaveBeenNotifiedTank == false) {
     Serial.println("Critical tank fill reached.");
-    notifyUsers();
-    usersHaveBeenNotified = true;
+    notifyUsers("Stille Wasser sind zwar tief, aber der Wasserstand im Tank wird langsam zu niedrig.");
+    usersHaveBeenNotifiedTank = true;
   }
-  else if (tankFill > PUMP_VOLUME*2) {
-    usersHaveBeenNotified = false;
+  else if (tankFill > TANK_VOLUME*0.1) {
+    usersHaveBeenNotifiedTank = false;
   }
 }
 
 
 void waterPlant() {
+  if(tankFill < PUMP_VOLUME) {
+    Serial.println("ERROR: Tank is completely empty");
+    if(!usersHaveBeenNotifiedTankEmpty) {
+      notifyUsers("Nein, meine Suppe ess ich nicht. Aber ich will Wasser, denn der Tank ist leer.");
+      usersHaveBeenNotifiedTankEmpty = true;
+    }
+    return;
+  }
+  usersHaveBeenNotifiedTankEmpty = false;
   Serial.println("Watering the plant");
-  digitalWrite(PUMP_PIN, LOW);
-  delay(PUMP_INTERVAL);
   digitalWrite(PUMP_PIN, HIGH);
+  delay(PUMP_INTERVAL);
+  digitalWrite(PUMP_PIN, LOW);
   tankFill -= PUMP_VOLUME;
   writeVolume(tankFill);
+  lastMillisWatering = millis();
 }
 
 
@@ -123,7 +143,30 @@ int readMoisture(int amountReadings) {
     reading += analogRead(SENSOR_PIN);
     delay(50);
   }
-  return (int)reading/5;
+  reading = (int)reading/amountReadings;
+
+  if(readingAir == 0 && readingWater == 0 || readingAir == readingWater) {
+    Serial.println("ERROR: Sensor has to be calibrated!");
+    if(amountRegisteredUsers != 0 && !usersHaveBeenNotifiedCalibration) {
+      notifyUsers("Hey, bitte kalibriere den Sensor, indem du /calibrate schickst. Sonst kann ich leider nicht arbeiten.");
+      usersHaveBeenNotifiedCalibration = true;
+    }
+    return 100;
+  }
+  else {
+    usersHaveBeenNotifiedCalibration = false;
+  }
+  return map(reading, min(readingAir, readingWater), max(readingAir, readingWater), 0, 100);
+}
+
+int readSensorRaw(int amountReadings) {
+  // read value
+  int reading = 0;
+  for (int i = 0; i < amountReadings; i++) {
+    reading += analogRead(SENSOR_PIN);
+    delay(50);
+  }
+  return (int)reading/amountReadings;
 }
 
 
@@ -137,6 +180,7 @@ String waitingVerification = "";
 String waitingDeletion = "";
 String waitingThreshold = "";
 String waitingReset = "";
+String waitingCalibration = "";
 
 void updateBot() {
   int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
@@ -175,42 +219,66 @@ void handleMessage(String chatID, String text, String name) {
     if(waitingDeletion == chatID) {
       if(text == "Delete") {
         bot.sendMessage(chatID, "Schade. Vielleicht sehen wir uns nochmal.\nTschüss " + name);
-        waitingDeletion = "";
         deleteUser(chatID);
       }
       else{
         bot.sendMessage(chatID, "Vorgang abgebrochen.");
-        waitingDeletion = "";
       }
+      waitingDeletion = "";
     }
     else if(waitingThreshold == chatID) {
       int thresholdNew = text.toInt();
       // No valid conversion
       if(thresholdNew == 0) {
         bot.sendMessage(chatID, "Ich bin kein Magier und kann aus Text keine Zahl machen. Vorgang abgebrochen.");
-        waitingThreshold = "";
       }
       else {
         threshold = thresholdNew;
         writeMoistureThreshold(threshold);
-        waitingThreshold = "";
         bot.sendMessage(chatID, "Neuer Schwellwert gesetzt.");
       }
+      waitingThreshold == "";
     }
     else if(waitingReset == chatID) {
-      bot.sendMessage(chatID, "War nett euch kennengelernt zu haben. Führe Factory Reset durch. Bitte trenne mich kurz vom Strom.");
-      resetEEPROM();
+      if(text == "RESET") {        
+        bot.sendMessage(chatID, "War nett euch kennengelernt zu haben. Führe Factory Reset durch. Bitte trenne mich kurz vom Strom.");
+        resetEEPROM();
+      }
+      else {
+        bot.sendMessage(chatID, "Vorgang abgebrochen");
+      }
+      waitingReset = "";
+    }
+    else if(waitingCalibration == chatID) {
+      if(text == "CALIBRATE") {
+        Serial.println("DEBUG: Sensor calibration started");
+        bot.sendMessage(chatID, "Cool, lasset die Kalibrierung beginnen. Bitte halte dafür den Sensor an die Luft. Stelle sicher, dass dieser trocken ist. Ich melde mich in 20s wieder");
+        delay(20000);
+        readingAir = readSensorRaw(10);
+        Serial.println((String)"DEBUG: readingAir: " + readingAir);
+        writeReadingAir(readingAir);
+        bot.sendMessage(chatID, "Super. Stelle den Sensor nun bitte in ein Glas mit Wasser. Ich melde mich in 20s wieder");
+        delay(20000);
+        readingWater = readSensorRaw(10);
+        Serial.println((String)"DEBUG: readingWater: " + readingWater);
+        writeReadingWater(readingWater);
+        bot.sendMessage(chatID, "Perfekt. Kalibrierung abgeschlossen");
+      }
+      else {
+        bot.sendMessage(chatID, "Vorgang abgebrochen");
+      }
+      waitingCalibration = "";
     }
 
     else if(text == "/status") {
       int fillPercentage = (int)((tankFill*100)/TANK_VOLUME);
-      bot.sendMessage(chatID, (String)"Die Tankfüllung liegt bei " + fillPercentage + "%.\nDer Messwert bei " + readMoisture(2) + ".");
+      bot.sendMessage(chatID, (String)"Tankfüllung: " + fillPercentage + "%.\nFeuchtigkeit: " + readMoisture(2) + "%.\nSchwellwert: " + threshold + "%");
     }
     else if(text == "/secret") {
       bot.sendMessage(chatID, "Hier kommt der Schlüssel. Teile ihn nicht mit jedem!");
       bot.sendMessage(chatID, SECRET);
     }
-    else if(text == "/deleteMe") {
+    else if(text == "/deleteme") {
       bot.sendMessage(chatID, "ACHTUNG\nBestätige das Löschen, indem du \"Delete\" schickst.");
       waitingDeletion = chatID;
     }
@@ -218,10 +286,7 @@ void handleMessage(String chatID, String text, String name) {
       bot.sendMessage(chatID, "Wie du befiehlst. Ich gieße!");
       waterPlant();
     }
-    else if(text == "/sensorRaw") {
-      bot.sendMessage(chatID, (String)"Der Messwert des Sensors liegt bei " + readMoisture(2) + ".");
-    }
-    else if(text == "/setThreshold") {
+    else if(text == "/setthreshold") {
       bot.sendMessage(chatID, (String)"Bitte gib den neuen Schwellwert für die Bewässerung ein. Der Schwellwert ist " + threshold + ", der Messwert liegt bei " + readMoisture(2) + ".");
       waitingThreshold = chatID;
     }
@@ -234,16 +299,21 @@ void handleMessage(String chatID, String text, String name) {
       tankFill = TANK_VOLUME;
       writeVolume(tankFill);
     }
+    else if(text == "/calibrate") {
+      bot.sendMessage(chatID, (String)"Für die Kalibrierung benötigst du ein Gefäß mit Wasser. Hole eins und bestätige die Durchführung der Kalibrierung, indem du \"CALIBRATE\" schickst.");
+      bot.sendMessage(chatID, (String)"Die aktuellen Werte liegen bei:\nMesswert Luft: " + readingAir + "\nMesswert Wasser: " + readingWater);
+      waitingCalibration = chatID;
+    }
     else {
       bot.sendMessage(chatID, "Der Befehl ist mit leider nicht bekannt.");
     }
   }
 }
 
-void notifyUsers() {
+void notifyUsers(String message) {
   Serial.println("DEBUG: Notifying users");
   for(int i=0; i<amountRegisteredUsers; i++) {
-    bot.sendMessage(users[i], "Stille Wasser sind zwar tief, aber der Wasserstand im Tank wird langsam zu niedrig.");
+    bot.sendMessage(users[i], message);
   }
 }
 
